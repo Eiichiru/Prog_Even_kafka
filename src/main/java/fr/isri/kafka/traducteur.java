62221d186608502bcd38639be94723996399463c
root <root@isri-19.israd.etud.u-picardie.fr>
2023-05-30 12:50:34 +0200

    Opt2 Fini

--- src/main/java/fr/isri/kafka/traducteur.java
+++ src/main/java/fr/isri/kafka/traducteur.java
@@ -1,75 +1,58 @@
 package fr.isri.kafka;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.ValueMapper;
 
-import java.time.Duration;
-import java.util.Arrays;
 import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
 
-public class traducteur
+public class traducteur {
 
-{
-    private static final Logger log = LoggerFactory.getLogger(producer.class);
+    public static void main(String[] args) throws Exception {
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-tempconvert");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
-    public static void main(String[] args) throws InterruptedException {
-        log.info("I am a Kafka traducteur");
+        final StreamsBuilder builder = new StreamsBuilder();
 
-        String bootstrapServersIN = "127.0.0.1:9092";
-        String groupIdIN = "my-fourth-application";
-        String topicIN = "temp_Celcius";
-
-        // create consumer configs
-        Properties propertiesIN = new Properties();
-        propertiesIN.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServersIN);
-        propertiesIN.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        propertiesIN.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        propertiesIN.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupIdIN);
-        propertiesIN.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-
-        //create producer configs
-        Properties propertiesOUT = new Properties();
-        propertiesOUT.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        propertiesOUT.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        propertiesOUT.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-
-         // create consumer
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(propertiesIN);
-
-        // create producer
-        KafkaProducer<String, String> producer = new KafkaProducer<>(propertiesOUT);
-
-
-        // subscribe consumer to our topic(s)
-        consumer.subscribe(Arrays.asList(topicIN));
-           // poll for new data
-        while(true){
-            ConsumerRecords<String, String> records =
-                    consumer.poll(Duration.ofMillis(100));
-
-            for (ConsumerRecord<String, String> record : records){
-
-                // create a producer record
-                ProducerRecord<String, String> producerRecord = new ProducerRecord<>("temp_Fahrenheit", String.valueOf(Integer.valueOf(record.value()) * (9/5) + 32));
-                // send data - asynchronous
-                producer.send(producerRecord);
-
-                // flush data - synchronous
-                producer.flush();
-
-                System.out.println("traduit et envoyé");
-
-                Thread.sleep(300);
+        KStream<String, String> source = builder.stream("temp_Celsius");
+        KStream<String, String> convertedSource = source.mapValues(new ValueMapper<>() {
+            @Override
+            public String apply(String value) {
+                System.out.println("Reçu, converti et envoyé");
+                double tempCelsius = Double.parseDouble(value);
+                double tempFahrenheit = tempCelsius * 9.0 / 5.0 + 32.0;
+                return Double.toString(tempFahrenheit);
             }
+        });
+        convertedSource.to("temp_Fahrenheit");
+
+        final Topology topology = builder.build();
+        final KafkaStreams streams = new KafkaStreams(topology, props);
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        // attach shutdown handler to catch control-c
+        Runtime.getRuntime().addShutdownHook(new Thread("streams-shutdown-hook") {
+            @Override
+            public void run() {
+                streams.close();
+                latch.countDown();
+            }
+        });
+
+        try {
+            streams.start();
+            latch.await();
+        } catch (Throwable e) {
+            System.exit(1);
         }
+        System.exit(0);
     }
 }
